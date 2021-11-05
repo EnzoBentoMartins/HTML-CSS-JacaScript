7a0bcee7ba1e3923b449d26f4ec488c19fe4fb0d
Enzo Bento Martins <86625455+EnzoBentoMartins@users.noreply.github.com>
2021-11-05 16:00:07 -0300

    Add files via upload

---
+++ HoraDoDia/js/ex01.js
@@ -0,0 +1,28 @@
+function loading() {
+    var body = window.document.getElementById('body')
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById("foto")
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    hora = 10
+    if (minuto < 10){
+        msg.innerHTML = `Agora são ${hora}:0${minuto}.`
+    }
+    else{
+        msg.innerHTML = `Agora são ${hora}:${minuto}.`
+    }
+
+    if (hora >= 0 && hora <= 12){
+        foto.style.backgroundImage = "url('images/manha.jpg')"
+        body.style.backgroundColor = '#ffdcbd'
+    }
+    else if (hora > 12 && hora <= 18){
+        foto.style.backgroundImage = "url('images/tarde2.jpg')"
+        body.style.backgroundColor = '#ff7038'
+    }
+    else{
+        foto.style.backgroundImage = "url('images/noite2.jpg')"
+        body.style.backgroundColor = '#1f1957'
+    }
+}
